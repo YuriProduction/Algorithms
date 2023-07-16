bbb0a3a850d721de3ed1c03f9f2c96ea99f9b654
Jurex <jurexda@yandex.ru>
2023-07-16 18:15:39 +0500

    New branch commit!

---
+++ Python_Lections/collections_.py
@@ -0,0 +1,22 @@
+cortezh = (100,)
+[first] = cortezh
+print(first)
+print([1, 2, 3, 4, 5][::2])
+print(tuple(reversed((1, 2, 3))))  # reverse не выделяет память
+print((1, 2, 3)[-1::-1])  # а здесь создается новый кортеж
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr[::2] = [55, 66, 77, 88, 99]
+print("arr: ", arr, sep='\t')
+from collections import namedtuple
+
+# фабрика классов, для использования легковесных классов
+Person = namedtuple("Person", ["name", "age"])
+p1 = Person("Gregory", "57")
+print(p1.age)
+
+# reversed - некий ленивый объект перечисляющий элементы в обратном порядке
+print((1, 2, 4) > (1, 2, 3, 4))  # сначала по первой координате, затем по второй и тд
+
+temp_list = [(1, 2), (1, 3), (2, 2)]
+temp_list.sort(key=lambda x: (x[1], x[0]), reverse=True)
+print(temp_list)
